ce6417251238ce58d23ea04595b7ce7b2c8d9b0c
v.mironov <vlmironoff@gmail.com>
2018-05-04 11:35:03 -0400

    Added recipe ingredients property S10-L110

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable()
 export class RecipeService {
@@ -7,8 +8,24 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Baked Apples', 'Baked apples recipe wording here', 'https://www.simplyrecipes.com/wp-content/uploads/2014/11/baked-apples-horiz-a-1800.jpg'),
-    new Recipe('Salmon Wellington', 'Salmon Wellington recipe wording here', 'http://tastykitchen.com/recipes/wp-content/uploads/sites/2/2016/09/Salmon-Wellington-by-Yummy-Addiction-410x274.jpg')
+    new Recipe(
+      'Baked Apples',
+      'Baked apples recipe wording here',
+      'https://www.simplyrecipes.com/wp-content/uploads/2014/11/baked-apples-horiz-a-1800.jpg',
+      [
+        new Ingredient('Salmon', 1),
+        new Ingredient('Dough', 3)
+      ]
+    ),
+    new Recipe(
+      'Salmon Wellington',
+      'Salmon Wellington recipe wording here',
+      'http://tastykitchen.com/recipes/wp-content/uploads/sites/2/2016/09/Salmon-Wellington-by-Yummy-Addiction-410x274.jpg',
+      [
+        new Ingredient('Apples', 6),
+        new Ingredient('Cinnamon', 10)
+      ]
+    )
   ];
 
   constructor() { }
