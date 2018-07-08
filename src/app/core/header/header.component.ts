11d4c19c39f72b74de49bf23b62c62304783ce69
v.mironov <vlmironoff@gmail.com>
2018-07-08 19:33:45 -0400

    Using authentication S24-L325

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,18 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataStorageService} from '../../shared/data-storage.service';
 import {RecipeService} from '../../recipes/recipe.service';
 import {AuthService} from '../../auth/auth.service';
+import {Store} from '@ngrx/store';
+import * as fromApp from '../../store/app.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  authState: Observable<fromAuth.State>;
 
   constructor(
      public dataStorageService: DataStorageService,
      public recipeService: RecipeService,
-     public authService: AuthService) {}
+     public authService: AuthService,
+     public store: Store<fromApp.AppState>) {}
+
+     ngOnInit() {
+        this.authState = this.store.select('auth');
+     }
 
   onSaveData() {
     this.recipeService.pushRecipes().subscribe(
