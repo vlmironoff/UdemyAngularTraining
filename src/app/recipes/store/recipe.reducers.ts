72c4bc6c471abc9ea3f41c2b1356af12bdcfdcc4
v.mironov <vlmironoff@gmail.com>
2018-09-03 01:12:01 -0400

    Adding recipe reducers S24 - L349

--- src/app/recipes/store/recipe.reducers.ts
+++ src/app/recipes/store/recipe.reducers.ts
@@ -1,4 +1,6 @@
 import {Recipe} from '../recipe.model';
+import * as RecipeActions from '../store/recipe.actions';
+import {s} from '@angular/core/src/render3';
 
 export interface FeatureState {
   recipes: State;
@@ -12,6 +14,25 @@ const initialState: State = {
   recipes: []
 };
 
-export function recipeReducer(state = initialState , action) {
-  return state;
+export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions) {
+  switch (action.type) {
+    case (RecipeActions.SET_RECIPES):
+      return {...state, recipes: [...action.payload]};
+
+    case (RecipeActions.ADD_RECIPE):
+      return {...state, recipes: [...state.recipes, action.payload]};
+
+    case (RecipeActions.UPDATE_RECIPE):
+      const recipe = state.recipes[action.payload.index];
+      const updatedRecipe = {...recipe, ...action.payload.updatedRecipe};
+      const recipes = [...state.recipes];
+      recipes[action.payload.index] = updatedRecipe;
+      return {...state, recipes: recipes};
+    case (RecipeActions.DELETE_RECIPE):
+      const oldRecipes = [...state.recipes];
+      oldRecipes.splice(action.payload, 1);
+      return {...state, recipes: oldRecipes};
+    default:
+        return state;
+  }
 }
